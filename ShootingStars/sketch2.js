438f784973f74e4c03fd9f5d431f656debcc5bd5
Tai-Him YIM <alvisyim@gmail.com>
2018-01-15 10:24:16 +0800

    fffffffffghjk;

--- ShootingStars/sketch2.js
+++ ShootingStars/sketch2.js
@@ -11,9 +11,9 @@ var missile01SizeX = 20;
 var missile01SizeY = 27;
 var missile01Move = false;
 
-var rock01LocationY = 0;
-//var rockState = "111";
+
 var rock01SizeY = 200/490*370;
+var rock01LocationY;
 
 var inPos;
 var bg01;
@@ -21,15 +21,11 @@ var ship01;
 var dottedLine;
 var rock01;
 
+var rock01Appear1 = false;
+var rock01Appear2 = false;
+var rock01Appear3 = false;
 
-
-
-var rock01Appear1 = true;
-var rock01Appear2 = true;
-var rock01Appear3 = true;
-
-var missile01Contact = false;
-
+var correctAnswer;
 
 
 function preload() {
@@ -47,6 +43,8 @@ function preload() {
 function setup () {
   createCanvas(600,400);
   textAlign(CENTER);
+
+  rock01LocationY = -rock01SizeY;
 
   ship01LocationX = (width/2)-(ship01SizeX/2);
   ship01LocationY = height-ship01SizeY-10;
@@ -66,6 +64,7 @@ function setup () {
 function draw() {
   background(255);
   bg();
+  rockInit();
   ifs();
   rock();
   keyFunction();
@@ -84,15 +83,22 @@ function bg() {
   image(dottedLine,0,(height-ship01SizeY-10)-10-5,600,0+10);
 }
 
-function ifs() {
-  if (missile01Move == true){
-    //missile Speed
-    missile01LocationY -= 10;
-    image(missile01,missile01LocationX, missile01LocationY,missile01SizeX, missile01SizeY);
-  } else if (missile01Move == false) {
-  }
+function rockInit(){
+////////////////////////////////////////////////////////////////////////////////////rock01Appear1 == ture
+    if (missile01Move == true){
+      //missile Speed
+      missile01LocationY -= 10;
+      image(missile01,missile01LocationX, missile01LocationY,missile01SizeX, missile01SizeY);
+    } else if (missile01Move == false) {
+    }
+}
 
-////////////////////////////// missile in position //////////////////////////////////////
+function ifs() {
+  if (rock01Appear1 == false && rock01Appear3 == false && rock01Appear3 == false ) {
+    rockInit();
+  }
+// missile in position
+{
   if (missile01LocationX + (missile01SizeX/2) >= 0 && missile01LocationX + (missile01SizeX/2) < 200){
     inPos = "1";
   }else if (missile01LocationX + (missile01SizeX/2) >= 200 && missile01LocationX + (missile01SizeX/2) < 400){
@@ -100,50 +106,96 @@ function ifs() {
   }else if (missile01LocationX + (missile01SizeX/2)>= 400 && missile01LocationX + (missile01SizeX/2) < 600){
     inPos = "3";
   }
-/////////////////////////////////////////////////////////////////////////////////////
-
-
+}
 
 //missile  contacts the rock line
+{
   if ((missile01LocationY < rock01LocationY + rock01SizeY) && missile01Move == true) {
-    missile01Contact = true;
     if (inPos == "1"){
-      rock01Appear1 = false;
+      if (rock01Appear1 == true ){
+        rock01Appear1 = false;
+        missile01Move = false;
+
+        if (correctAnswer == "1"){
+          rock01Appear1 = false;
+          rock01Appear2 = false;
+          rock01Appear3 = false;
+        }
+      } else {
+      }
     }
     if (inPos == "2"){
-      rock01Appear2 = false;
+      if (rock01Appear2 == true ){
+        rock01Appear2 = false;
+        missile01Move = false;
+
+        if (correctAnswer == "2"){
+          rock01Appear1 = false;
+          rock01Appear2 = false;
+          rock01Appear3 = false;
+        }
+      } else {
+      }
     }
     if (inPos == "3"){
-      rock01Appear3 = false;
+      if (rock01Appear3 == true ){
+        rock01Appear3 = false;
+        missile01Move = false;
+
+        if (correctAnswer == "3"){
+          rock01Appear1 = false;
+          rock01Appear2 = false;
+          rock01Appear3 = false;
+        }
+      } else {
+      }
     }
-    missile01Move = false;
   }
   if (missile01Move == true && missile01LocationY <=0){
     missile01Move = false;
   }
+  ///////////////////////////////////////////////////////////////////////////////
+}
 
-
-
-//////////////////////////////////////// reset ////////////////////////////////////////////
+//missile reset
+{
   if (missile01Move == false){
     missile01LocationX = (ship01LocationX+(ship01SizeX/2))-(missile01SizeX/2);
     missile01LocationY = height-missile01SizeY-60;
   }
-///////////////////////////////////////////////////////////////////////////////////
+}
 
-
-
+//loop initiale
+{
   if (rock01Appear1 == false && rock01Appear2 == false && rock01Appear3 == false) {
     //rock01Appear1 = true;
     //rock01Appear2 = true;
     //rock01Appear3 = true;
   }
+}
 
+//rock reset
+{
+  if (rock01Appear1 == false && rock01Appear2 == false && rock01Appear3 == false) {
+      resetStuff();
+  }
+}
 
-  if ((rock01LocationY+rock01SizeY >= (height-ship01SizeY-10)-10 + 5) && (rock01Appear1 == true || rock01Appear2 == true || rock01Appear3 == true)) {
+//gameMode
+{
+  if (rock01LocationY+rock01SizeY >= (height-ship01SizeY-10)-10 + 5) {
     gameOver();
   }
-  //if(missile01LocationY <=)
+}
+
+}
+
+function resetStuff(){
+    rock01Appear1 = true;
+    rock01Appear2 = true;
+    rock01Appear3 = true;
+    correctAnswer = int(random(1, 3.9999999999));
+    rock01LocationY = -rock01SizeY;
 }
 
 function rock () {
@@ -200,10 +252,8 @@ function test() {
   fill(225,0,0);
   text(inPos,0,0,100,100);
   text(missile01LocationX + missile01SizeX/2,0,20,100,100);
-  text(rock01Appear1,0,40,100,100);
-  text(rock01Appear2,0,60,100,100);
-  text(rock01Appear3,0,80,100,100);
-  text(missile01Contact,0,100,100,100);
+  text(missile01Move,0,40,100,100);
+  text(correctAnswer,0,60,100,100);
 }
 
 
