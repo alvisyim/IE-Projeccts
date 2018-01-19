f4bdbc97465ac7c897b476d7917078844b986a31
Tai-Him YIM <alvisyim@gmail.com>
2018-01-19 10:09:24 +0800

    jkl;'

--- ShootingStars/mainMenu.js
+++ ShootingStars/mainMenu.js
@@ -2,7 +2,7 @@ function preload() {
   bg01 = loadImage("bg01.jpg");
   //320x480
   logo = loadImage("shootingStarsLogo.png");
-  //6653x4048
+  //6652x2889
 }
 
 function setup () {
@@ -12,7 +12,7 @@ function setup () {
 function draw() {
   background(225);
   image(bg01,0,0,width, 600/320*480);
-  image(logo,170,-50,6653/12,4048/12);
+  image(logo,170,-50,6652/12,2889/12);
 
   playButton = createButton("Play >>");
   playButton.size(180,65);
